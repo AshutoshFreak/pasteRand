e67fbbac8248e0eaec6c9e27c6827d3dee50b90b
Furkan <furkantahaonder@gmail.com>
2019-08-23 23:29:04 +0300

    templates fixed

--- paste/forms.py
+++ paste/forms.py
@@ -1,9 +1,14 @@
+from django import forms
 from django.forms import ModelForm
 
 from .models import PasteFile
 
 
 class PasteForm(ModelForm):
+    content = forms.CharField(
+        required=False, widget=forms.Textarea(attrs={"rows": 15, "cols": 80})
+    )
+
     class Meta:
         model = PasteFile
         fields = ("title", "content")
