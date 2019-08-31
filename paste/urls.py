8b896057f1e58e29cb380f46989f954e168a7177
Furkan <furkantahaonder@gmail.com>
2019-08-31 11:52:07 +0300

    comment structure moved to detail view

--- paste/urls.py
+++ paste/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from . import views
 from .views import Detail, Index
 
 app_name = "paste"
@@ -8,5 +7,4 @@ app_name = "paste"
 urlpatterns = [
     path("", view=Index.as_view(), name="index"),
     path("p/<str:slug>/", view=Detail.as_view(), name="detail"),
-    path("post/<str:slug>/comment/", views.add_comment, name="add_comment"),
 ]
--- paste/views.py
+++ paste/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
+from django.shortcuts import get_object_or_404,render,redirect
 from django.views.generic import CreateView, DetailView
 
 from .forms import CommentForm, PasteForm
@@ -12,7 +12,7 @@ class Index(CreateView):
     def form_valid(self, form):
         instance = form.save()
         form.save()
-        return HttpResponseRedirect(instance.get_absolute_url())
+        return redirect(instance.get_absolute_url())
 
 
 class Detail(DetailView):
@@ -22,25 +22,25 @@ class Detail(DetailView):
     def get(self, request, slug):
         paste_obj = get_object_or_404(PasteFile, slug=slug)
         comments = Comment.objects.filter(paste_file_id=paste_obj.pk)
+        form = CommentForm(request.POST)
         paste_content = {
             "title": paste_obj.title,
             "content": paste_obj.content,
             "date_time": paste_obj.date_time,
             "pk": paste_obj.pk,
             "comments": comments,
+            "form":form,
         }
         return render(request, self.template_name, paste_content)
 
-
-def add_comment(request, pk):
-    post = get_object_or_404(PasteFile, pk=pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save()
-            comment.paste_file = post
-            comment.save()
-            return HttpResponseRedirect(post.get_absolute_url())
-    else:
-        form = CommentForm()
-    return render(request, "paste/comment_form.html", {"form": form})
+    
+    def post(self,request,slug):        
+        post = get_object_or_404(PasteFile, slug=slug)
+        if request.method == "POST":
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save()
+                comment.paste_file = post
+                comment.save()
+                return redirect(post.get_absolute_url())
+        return render(request, self.template_name, paste_content)
